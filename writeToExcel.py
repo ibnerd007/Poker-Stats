8f02d343cd8da08eee3f69c94abbc7a4e5dd19b1
ibnerd007 <83733064+ibnerd007@users.noreply.github.com>
2021-05-13 15:49:14 -0400

    Added Excel functionality
    Stats for one session will now fill an Excel spreadsheet, filling the tables as necessary.
    
    However, data ranges will not autofit, the series are renamed to default (Series1, Series2, ...), and macro-enabled workbooks (*.xlsm) will not raise an error with openpyxl, but will corrupt the file and make it unreadable to Excel.

---
+++ writeToExcel.py
@@ -0,0 +1,51 @@
+import openpyxl
+
+def writeToExcel(vpipM, pfrM, tbpM, afM, afqM, wtsdM, wasdM, mwas, mwbs, 
+			     ledgerM, staticIDs, playerIDs, playerDict, handsPlayed, bestHandsM):
+	
+	# wb = openpyxl.Workbook() # create new workbook
+	wb_path = r'Outputs\stats.xlsm'
+
+	wb = openpyxl.load_workbook(wb_path) # load existing workbook
+
+	sheet = wb['Stats-this session'] # access sheet
+
+	# for bankroll in bankrolls:
+	# 	sheet.append(bankroll) # append to end of data
+
+	# sheet.insert_rows(1, 2) # Before 1st row, insert 2 columns
+
+	# row = player
+	# column = stat index
+	# value = stat value
+
+	for i in range(len(playerIDs)): # Fill out player names first
+		sheet.cell(row=i + 2, column=1, value=playerDict[playerIDs[i]])
+
+	# Next, fill ledger stats
+	for player in range(len(playerIDs)): # cols
+		for stat in range(len(ledgerM[0])): # rows
+			sheet.cell(row=player + 2, column=stat + 2, value=ledgerM[player][stat]) # averaged positions
+			# print(ledgerM[player][stat])
+
+
+	tdStats = [vpipM, pfrM, tbpM, afM, afqM, wtsdM, wasdM]
+	moneyStats = [mwas, mwbs]
+
+	# Fill Excel spreadsheet with percent stat data
+	for stat in range(len(tdStats)): # rows
+		for player in range(len(playerIDs)): # cols
+			sheet.cell(row=player + 2, column=stat + 6, value=tdStats[stat][player][2]) # averaged positions
+
+	# Fill Excel spreadsheet with monetary stat data
+	for stat in range(len(moneyStats)):
+		for player in range(len(playerIDs)):
+			sheet.cell(row=player + 2, column=stat + 13, value=moneyStats[stat][player]) # averaged positions
+
+
+
+	wb.save(wb_path)
+
+
+
+
