3df6d9ea87f7d157acda237a553b5f616091d382
ibnerd007 <83733064+ibnerd007@users.noreply.github.com>
2021-05-28 12:33:40 -0400

    Ben added to dictionary

--- calcNetStacks.py
+++ calcNetStacks.py
@@ -4,14 +4,12 @@ import time
 def calcNetStacks(sessionStacks):
 
 	stacks = copy.deepcopy(sessionStacks)
-	print(stacks)
 
 	prevNumPlayers = 0
 	startingStacks = []
 	net = [[]]
 
 	for i in range(len(stacks)):
-		print('i = ', i)
 		numPlayers = len(stacks[i])
 
 		if numPlayers > prevNumPlayers: # 1 or more people joined the session
@@ -19,8 +17,6 @@ def calcNetStacks(sessionStacks):
 
 			for k in range(playersAdded):
 				stackIdx = prevNumPlayers+k
-				print('stackIdx: ', stackIdx)
-				time.sleep(0.01)
 				startingStacks.append(stacks[i][stackIdx]) # append the player's starting stack from the end of stacks
 
 		for j in range(numPlayers):
