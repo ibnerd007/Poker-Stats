6aabf27e62a2404aa3fe3aa308e199c28422a9fd
ibnerd007 <83733064+ibnerd007@users.noreply.github.com>
2021-06-07 15:09:55 -0400

    Filled average stats for combined
    All stats for all sessions of all poker types are now filled.
    
    Total, we have about 2,500 hands played together. Good memories, and hopefully many more to come.

--- runPokerStats.py
+++ runPokerStats.py
@@ -1,6 +1,6 @@
 # set date of session & poker type desired (Holdem, PLO, or both)
 date = '5 31'
-handTypeDesired = 'PLO' # can be NL, PLO, or combined
+handTypeDesired = 'combined' # can be NL, PLO, or combined
 
 # NLdates = ['4 16', '4 19', '4 24', '4 29', '5 04', '5 13', '5 17', '5 20', '5 24',
 # 		   '5 27', '5 31']
