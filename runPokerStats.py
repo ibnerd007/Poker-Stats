8cca40fceeb813612f4c2a4ca2a3a606e55f5c85
ibnerd007 <83733064+ibnerd007@users.noreply.github.com>
2021-06-16 15:17:27 -0400

    Working on GUI
    Tkinter GUI begun. Also working on a quick function that returns a list of dates from Excel logs stored in the Logs directory. This allows the program GUI to only allow user to select dates that have associated log and ledger files.

--- runPokerStats.py
+++ runPokerStats.py
@@ -1,11 +1,65 @@
-import runpy
+import os
 
-date = '0614'
+from poker_stats import pokerStats
+from tkinter import *
+
+date = '0610'
 handTypeDesired = 'NL'
 
-runpy.run_path('pokerStats.py')
+handTypes = ('combined', 'NL', 'PLO')
 
-# date = '0504'
-# handTypeDesired = 'NL'
+# date = input('Enter date:')
+# handType = input('Enter poker type: (NL or PLO)')
 
-# runpy.run_path('pokerStats.py')
+# pokerStats(date, handType)
+
+# input('Press enter to exit.')
+
+def run():
+
+	# Create object
+	window = Tk()
+
+	# Adjust size
+	window.geometry("400x400")
+
+	# Now that variables are defined, run Poker Stats
+	def runPokerStats():
+		date = clicked.get()
+		pokerStats(date, 'NL')
+
+	# Dropdown menu options
+	logs = os.listdir('Logs')
+	options = [
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday",
+	"Sunday"
+	]
+
+	# datatype of menu text
+	clicked = StringVar()
+
+	# initial menu text
+	clicked.set(options[-1]) # set to penultimate element (most recent date) in list
+
+	# Create Dropdown menu
+	drop = OptionMenu( window , clicked , *options )
+	drop.pack()
+
+	# Create button, it will change label text
+	button = Button( window , text = "Run" , command = runPokerStats ).pack()
+
+	# Create Label
+	label = Label( window , text = " " )
+	label.pack()
+
+	# Execute tkinter
+	window.mainloop()
+
+
+
+run()
---
+++ testOS.py
@@ -0,0 +1,14 @@
+# Python program to explain os.getcwd() method
+		
+# importing os module
+import os
+	
+# Get the current working
+# directory (CWD)
+list = os.listdir('Logs')
+	
+# Print the current working
+# directory (CWD)
+print("Files in directory:", list)
+
+
